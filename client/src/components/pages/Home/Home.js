1c80458d8db945707caa39754706914400929933
Charles <charlesmbrady@gmail.com>
2019-08-21 19:44:33 -0400

    redid home

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -10,20 +10,22 @@ function Home() {
 
 
     return (
-        <Container>
-            <Row>
-                <Col>
-                    <input id="search-input" placeholder="Type search term here"></input>
-                </Col>
-            </Row>
-          
-            <Row>
-                <Col>
-                    <h1></h1>
-                </Col>
-            </Row>
+        <div>
+            <Container>
+                <Row>
+                    <Col>
+                        <input id="search-input" placeholder="Type search term here"></input>
+                    </Col>
+                </Row>
 
-        </Container>
+                <Row>
+                    <Col>
+                        <h1></h1>
+                    </Col>
+                </Row>
+
+            </Container>
+        </div>
     );
 }
 
