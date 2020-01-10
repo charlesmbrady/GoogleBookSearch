d1baf94094e7981a7eecbad0c731ef0111f6318b
Charles <charlesmbrady@gmail.com>
2020-01-10 15:43:17 -0500

    removed everythign but card image for the books

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -75,15 +75,6 @@ function Home() {
                   }
                   alt="Card image cap"
                 />
-                {/* <CardBody>
-                  <CardTitle>Card title</CardTitle>
-                  <CardSubtitle>Card subtitle</CardSubtitle>
-                  <CardText>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </CardText>
-                  <Button>Button</Button>
-                </CardBody> */}
               </Card>
             </div>
           ))}
