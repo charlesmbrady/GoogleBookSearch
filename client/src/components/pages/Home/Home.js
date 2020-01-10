52e055f61e83dcd80bd364d744c672f725cfc2b8
Charles <charlesmbrady@gmail.com>
2020-01-10 13:19:25 -0500

    only run search query after the first load

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -13,29 +13,68 @@ import {
   CardText,
   Container,
   Row,
-  Col
+  Col,
+  CardImg,
+  CardSubtitle
 } from "reactstrap";
 import googleAPI from "../../../utils/googleAPI";
 
 function Home() {
+  const [query, setQuery] = useState("");
+  const [count, setCount] = useState(0);
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    if (count !== 0) {
+      googleAPI.searchTitles(query).then(res => {
+        console.log("here are search resultts" + res);
+      });
+    }
+  }, [count]);
+
+  const search = query => {
+    setQuery(query);
+    setCount(count + 1);
+  };
+
   return (
     <div>
       <Container>
-        <Row>
-          <Col>Saved</Col>
+        <Row className="row justify-content-end">
+          <Col className="col-md-auto col-sm-6">View Saved Books</Col>
         </Row>
         <Row className="row justify-content-md-center">
           <Col className="col-md-6 col-sm">
             <InputGroup className="input-group">
               <Input
                 className="input"
+                name="query"
+                onChange={e => search(e.target.value)}
                 placeholder="Search the GoogleBooks API by title/keyword"
               />
             </InputGroup>
           </Col>
         </Row>
         <Row>
-          <Col>books here</Col>
+          <Col className="col-sm-3">
+            <Card>
+              <CardImg
+                top
+                width="100%"
+                src="/assets/318x180.svg"
+                alt="Card image cap"
+              />
+              <CardBody>
+                <CardTitle>Card title</CardTitle>
+                <CardSubtitle>Card subtitle</CardSubtitle>
+                <CardText>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </CardText>
+                <Button>Button</Button>
+              </CardBody>
+            </Card>
+          </Col>
         </Row>
       </Container>
     </div>
